290321a57d33f1b0b268c2353e6e61748c800476
Manuel <manuel@yoast.com>
2019-05-09 13:30:24 +0200

    complete task 3 step 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,31 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
   });
 
-  // Load tasks here.
+	grunt.loadNpmTasks('grunt-string-replace');
 
   // Define aliases here.
-  grunt.registerTask('default', 'My default task description', function() {
-    grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
-  });
+  grunt.registerTask('default', [ 'update-version' ] );
+  grunt.registerTask('update-version', "Update version number", function( newVersion ) {
+    const filesAndRegexes = [
+        {
+          file: "source-files/index.php",
+	      regex: /(\* version:\s+)(\d\.\d\.\d(-RC\d+)*)(\n)/ig,
+        },
+	    {
+	      file: "source-files/classes/class.php",
+          regex: /(\t+const\sversion\s=\s\")(\d\.\d\.\d(-RC\d+)*)(\";\n)/ig,
+	    },
+	    {
+		  file: "source-files/js/script.js",
+		  regex: /(window\.plugin\.version\s=\s\")(\d\.\d\.\d(-RC\d+)*)(\";\n)/ig,
+	    }
+    ];
+
+    filesAndRegexes.forEach( ( combination ) => {
+      let file = grunt.file.read( combination.file );
+      file = file.replace( combination.regex, "$1" + newVersion + "$4" );
+      grunt.file.write( combination.file, file );
+      } );
+  } );
 
 };
