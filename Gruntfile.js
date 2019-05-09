59f4dc3bdc7538907c2e2f13670d16f2c0655610
Manuel <manuel@yoast.com>
2019-05-09 10:57:23 +0200

    implement copy, sass & compress

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,48 @@
 /*global module:false*/
+var sass = require('node-sass');
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+      copy: {
+	      main: {
+		      files: [
+			      {expand: true, cwd: './source-files', src:'**/*.js', dest: 'dist/assets/', filter: 'isFile'},
+			      {expand: true, src: ['./index.html'], dest: 'dist/', filter: 'isFile'},
+		      ],
+	      },
+      },
+	  sass: {
+		  options: {
+			  implementation: sass,
+			  outputStyle: 'expanded',
+		  },
+		  dist: {
+			  files: {
+				  './dist/assets/style.css': './source-files/style.scss'
+			  }
+		  }
+	  },
+	  compress: {
+		  main: {
+			  options: {
+				  archive: 'archive.zip'
+			  },
+			  files: [
+				  {src: ['./dist/**'], dest: '/'},
+			  ]
+		  }
+	  }
   });
 
   // Load tasks here.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 
-  // Define aliases here.
-  grunt.registerTask('default', 'My default task description', function() {
-    grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
-  });
+	grunt.registerTask('default', ['copy', 'sass', 'compress']);
 
 };
