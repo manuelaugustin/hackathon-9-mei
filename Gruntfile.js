4b16e2b2f7d3b069828725e5bfd1fd5b786ae7b3
Manuel <manuel@yoast.com>
2019-05-09 11:34:56 +0200

    complete task 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,55 @@
 /*global module:false*/
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+	  copy: {
+		  main: {
+			  files: [
+				  {expand: true, cwd: 'source-files/', src: ['**'], dest: 'dist/assets'},
+			  ],
+		  },
+	  },
+	  uglify: {
+		  options: {
+			  mangle: true
+		  },
+		  my_target: {
+			  files: [{
+				  expand: true,
+				  cwd: 'dist/assets/js',
+				  src: '*.js',
+				  dest: 'dist/assets/js'
+			  }]
+		  }
+	  },
+	  sass: {
+		  options: {
+			  implementation: sass,
+		  },
+		  dist: {
+			  files: {
+				  'dist/assets/css/conditional-style.css': 'dist/assets/css/conditional-style.scss',
+				  'dist/assets/css/style.css': 'dist/assets/css/style.scss'
+			  }
+		  }
+	  },
+	  clean: {
+		  obsolete_scss: ['dist/assets/css/*.scss']
+	  }
   });
 
   // Load tasks here.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // Define aliases here.
-  grunt.registerTask('default', 'My default task description', function() {
-    grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
-  });
+	// Define aliases here.
+  grunt.registerTask('artifact', [ 'copy', 'uglify', 'sass', 'clean' ] );
 
 };
